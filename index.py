15208bb494edf957d33d94cb51ac8a06bbc58e60
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-08 19:48:17 -0300

    Começando a analise exploratoria dos dados

--- index.py
+++ index.py
@@ -18,3 +18,45 @@ from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 # Salvar um modelo
 import pickle
+
+
+# Tratamento dos Dados
+
+# Dicionário de Dados
+
+
+# Que pergunta(s) você está tentando resolver?
+# Que tipo de dados temos e como tratamos os diferentes tipos?
+# O que está faltando nos dados e como você lida com isso?
+# Onde estão os valores outliers e por que você deveria se importar com eles?
+# Como você pode adicionar, alterar ou remover recursos para aproveitar melhor seus dados?
+
+df = pd.read_csv('datA/heart-disease.csv', sep=',', encoding='utf-8')
+print(df)
+
+# Linhas x colunas
+print(df.shape) 
+
+# Primeiras linhas
+print(df.head())
+
+# Ver a quantidade de cada valor na coluna alvo
+print(df['target'].value_counts())
+
+df['target'].value_counts().plot(kind="bar", color=["salmon", "lightblue"])
+plt.show()
+
+# Vendo demais informações sobre o dataset
+print(df.describe())
+
+print(df.info())
+
+print(df.isna().sum())
+
+# Comparação de colunas entre si para poder achar padrões
+
+# Sex and target
+print(df['sex'].value_counts()) # muito mais masculino do que feminino
+
+comparation_sex_target = pd.crosstab(df['target'], df['sex'])
+print(comparation_sex_target)
